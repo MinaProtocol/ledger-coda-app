e8bbe5ae95a3c014771a9720c7ef425d8aca782d
Rebekah Mercer <github@rbkhmrcr.com>
2020-02-04 18:33:34 -0800

    updates

--- cli/decode.py
+++ cli/decode.py
@@ -53,13 +53,13 @@ def split(in_bytes, request):
 
 def sig_encode(bs):
     r, s = split(bs, 'sig')
-    print(json.dumps({'field': r, 'scalar':s}))
+    print(json.dumps({'field': r, 'scalar': s}))
     return
 
 # versionbyte length = 1 byte
 def pk_encode(bpk):
     x, y = split(bpk, 'pk')
-    print(json.dumps({'x': b58x, 'y':b58y}))
+    print(json.dumps({'x': x, 'y': y}))
     return
 
 # b58_pk is version byte || sign bit || x coord
@@ -168,7 +168,10 @@ handled_keys = ['{"x": "115LFY15j2xqcFJXLYvScasi93wyTdmeKPiVgsfzfiBRctYgps77to5M
 hex_sig = b'000158edcd4c48a4045830e7e4228a0d824a3732ba6fb5424dfe313ddbf1cd4c53a4c527c7cc9767f5dcba62a1152ff677b7e5b0d93c408e8b39c66d368cd94682d168683f1492537b93fba8d137d007d812b52f92e456ebcb91177bba4b60a600001508027ae10bc81444d002258ab4a02abeba8666f618ef4175c2b0c1b4720b674521eb0b3221afa6d7b0bfb5ad825a35e74b8e44e94c833be10ce144e098e1e821a965d4f4a79499c8161744d2f58b7cc93536063df61c787f78464bab03'
 
 def b58_to_int(b58_bytes):
-    return int.from_bytes(base58.b58decode(b58_bytes), byteorder='big') % schnorr.p
+    return int.from_bytes(base58.b58decode(b58_bytes), byteorder='little') % schnorr.p
+
+def int_to_b58(ii):
+    return base58.b58encode(schnorr.bytes_from_int(ii))
 
 if __name__ == "__main__":
     assert base58.b58encode(b'hello') == b'Cn8eVZg'
@@ -177,3 +180,5 @@ if __name__ == "__main__":
     assert base58.b58decode(b'EUYUqQf') == b'world'
     assert base58.b58decode_check(b'2L5B5yqsVG8Vt') == b'hello'
     assert base58.b58decode(b'a') == b'!'
+    assert b58_to_int(b'113BbGK8KghQV7Agy8W9YDev1rnQWFncJNbX6FNk5xt3i85ArogCLyxcAFmtCQXdqW1N83mmzxPjTEbk2ZSJ2HBQXb296kVvCg5ntqkFQTj7W15dcccHbSc4YwFwhBiwQpx') == 11422395288969951266668318517247797610158769971697294414104000319354108053709789800993457876168384338057945990070182946671950057475934120004133141526633780465748593106560360725236740562531793133853926323126879462811998959299673
+    assert b58_to_int(b'113JiNkNmUXjoZFZkfB17UfkgU6qwXjseCJY7u8iTw8j9c5b3Gjh2YTkCU1XQVYS9r4xQEcUwz7AMtH247VgEBaweRQZRAqEYkYmMwzGPvPyfiiJ6DNFY93pv83T9zRCDvF') == 12065073978853608390151781695805078280181820006872152624266968017931029341197429495647513274590720850751139309265446457816736158553413796964167609808052788939737267906950034021628473817867647283771188497811472057195260597361304
